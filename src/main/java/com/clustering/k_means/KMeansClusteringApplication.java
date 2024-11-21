82c415ff054ac7d2adb4c8117e10b3ae44bfb6d6
DedliParkurKiller123 <t755631@gmail.com>
2024-11-21 20:58:32 +0200

    First commit

---
+++ src/main/java/com/clustering/k_means/KMeansClusteringApplication.java
@@ -0,0 +1,13 @@
+package com.clustering.k_means;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KMeansClusteringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KMeansClusteringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clustering/k_means/KMeansClusteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clustering.k_means;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KMeansClusteringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
