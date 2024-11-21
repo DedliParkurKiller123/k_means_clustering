760b63cefa4f144af96d14ea387e21d684a27073
DedliParkurKiller123 <t755631@gmail.com>
2024-11-21 22:22:44 +0200

    Added model and connect to db commit

--- src/main/java/com/clustering/k_means/models/Country.java
+++ src/main/java/com/clustering/k_means/models/Country.java
@@ -1,4 +1,21 @@
 package com.clustering.k_means.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "countries_db")
 public class Country {
+
+    @SequenceGenerator(
+            sequenceName = "seq_countries",
+            name = "seq_countries",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "seq_countries",
+            strategy = GenerationType.SEQUENCE
+    )
+    @Id
+    private Long idCountry;
+    private String nameOfCountry;
 }
