9ecfb98cbee762ebaba64abbfeca0df20f9a7259
DedliParkurKiller123 <t755631@gmail.com>
2024-11-23 13:55:22 +0200

    Added all needed field

--- src/main/java/com/clustering/k_means/models/Country.java
+++ src/main/java/com/clustering/k_means/models/Country.java
@@ -1,9 +1,14 @@
 package com.clustering.k_means.models;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "countries_db")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Country {
 
     @SequenceGenerator(
@@ -18,4 +23,17 @@ public class Country {
     @Id
     private Long idCountry;
     private String nameOfCountry;
+    private Double GPC;
+    private Double birthrate;
+    private Double deathrate;
+    private Double netMigration;
+    private Double infantMortality;
+    private Double literacy;
+    private Double phones;
+    private Double popDensity;
+    private Double industry;
+    private Double service;
 }
+
+
+//Australia ,"29000","12,14","7,51","3,98","4,69","100,0","565,5","2,6","0,262","0,7"
