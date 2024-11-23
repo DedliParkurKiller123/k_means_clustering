5d402dbaa178951219a1dbcd65d8e2a90f6ba107
DedliParkurKiller123 <t755631@gmail.com>
2024-11-23 15:07:41 +0200

    Implementation method parseCsv

--- src/main/java/com/clustering/k_means/services/CountryService.java
+++ src/main/java/com/clustering/k_means/services/CountryService.java
@@ -3,6 +3,8 @@ package com.clustering.k_means.services;
 import com.clustering.k_means.CsvRepresentation.CountriesCsvRepresentation;
 import com.clustering.k_means.models.Country;
 import com.clustering.k_means.repository.CountryRepository;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.bean.HeaderColumnNameMappingStrategy;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -14,6 +16,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +26,7 @@ public class CountryService {
 
     public Integer uploadCountries(MultipartFile file) {
         Set<Country> countries = parseCsv(file);
+        return null;
      }
 
     @SneakyThrows(IOException.class)
@@ -30,6 +34,40 @@ public class CountryService {
         try(Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))){
             HeaderColumnNameMappingStrategy<CountriesCsvRepresentation> strategy
                     = new HeaderColumnNameMappingStrategy<>();
+            strategy.setType(CountriesCsvRepresentation.class);
+            CsvToBean<CountriesCsvRepresentation> csvToBean
+                    = new CsvToBeanBuilder<CountriesCsvRepresentation>(reader)
+                    .withMappingStrategy(strategy)
+                    .withIgnoreEmptyLine(true)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            return csvToBean.parse()
+                    .stream()
+                    .map(csvLine -> Country.builder()
+                            .nameOfCountry(csvLine.getCountry())
+                            .GDP(csvLine.getGdpPerCapita())
+                            .birthrate(csvLine.getBirthrate())
+                            .deathrate(csvLine.getDeathrate())
+                            .netMigration(csvLine.getNetMigration())
+                            .infantMortality(csvLine.getInfantMortality())
+                            .literacy(csvLine.getLiteracy())
+                            .phones(csvLine.getPhonesPer1000())
+                            .popDensity(csvLine.getPopDensity())
+                            .industry(csvLine.getIndustry())
+                            .service(csvLine.getService())
+                            .build()
+                    ).collect(Collectors.toSet());
         }
     }
 }
+//private String nameOfCountry;
+//private Double GDP;
+//private Double birthrate;
+//private Double deathrate;
+//private Double netMigration;
+//private Double infantMortality;
+//private Double literacy;
+//private Double phones;
+//private Double popDensity;
+//private Double industry;
+//private Double service;
